e9bea3370a736db8be05afd854c76bd59155f66b
tnantoka <tnantoka@bornneet.com>
2016-12-27 21:38:50 +0900

    Add parse route

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Foundation
 
 let drop = Droplet()
 
@@ -6,6 +7,19 @@ drop.get { req in
     return try drop.view.make("welcome", [
         "url": "https://johnny:p4ssw0rd@www.example.com:443/script.ext;param=value?query=value#ref"
     ])
+}
+
+drop.get("parse") { req in
+    var context = [String : Node]()
+    context["url"] = ""
+    context["parsed"] = .array([])
+    // `req.query?["url"]?.string` does not work with complex URL
+    if let urlString = req.uri.query?.replacingOccurrences(of: "\\Aurl=", with: "", options: .regularExpression),
+        let url = URL(string: urlString) {
+        context["url"] = .string(urlString)
+        context["parsed"] = url.parsed
+    }
+    return try drop.view.make("welcome", context)
 }
 
 drop.run()
